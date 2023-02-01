919928a75f356a3186d9c751d1bbbcfb7e9fe128
Shruti Tyagi <123549951+shrutityagi541@users.noreply.github.com>
2023-02-01 23:55:27 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,20 @@
+// this is two pointer question i and j is pointer
+
+public class reversearray {
+    public static void main(String[] args) {
+        int arr[] = {8,5,7,6,3,2,1};
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j]= temp;
+            i++;
+            j--;
+        }
+        for(int k=0;k<n;k++){
+        System.out.println(arr[k]);
+        }
+    }
+}
